12dbc9e7aa45c1ff8f09693f3a5671e5ea6ded94
hiepnguyen-vizion <hiepma.1793@gmail.com>
2023-09-21 19:59:01 +0700

    add

--- Physical/moisture.py
+++ Physical/moisture.py
@@ -2,12 +2,14 @@ import time
 from Physical.physical import *
 
 class Moisture:
-    def __int__(self):
+    serial = ""
+    def __int__(self,ser):
         print("Moisture")
+        self.serial = ser
 
-    def readMoisture(self, ser):
-        serial_read_data(ser)
-        ser.write(soil_moisture)
+    def readMoisture(self):
+        serial_read_data(self.serial)
+        self.serial.write(soil_moisture)
         time.sleep(1)
-        print("Moisture",serial_read_data(ser))
-        return serial_read_data(ser)
+        print("Moisture",serial_read_data(self.serial))
+        return serial_read_data(self.serial)
--- Physical/temperature.py
+++ Physical/temperature.py
@@ -1,12 +1,14 @@
 import time
 from Physical.physical import *
 class Temperature:
-    def __int__(self):
+    ser = ""
+    def __int__(self, ser):
         print("Run Task do nhiet do")
+        self.ser = ser
 
-    def readTemperature(self, ser):
-        serial_read_data(ser)
-        ser.write(soil_temperature)
+    def readTemperature(self):
+        serial_read_data(self.ser)
+        self.ser.write(soil_temperature)
         time.sleep(1)
-        print("Temperature",serial_read_data(ser))
-        return serial_read_data(ser)
+        print("Temperature",serial_read_data(self.ser))
+        return serial_read_data(self.ser)
