565c75614003535b54975560340929915d45a9dd
hiepnguyen-vizion <hiepma.1793@gmail.com>
2023-09-28 19:07:10 +0700

    Create getPostData.py

---
+++ getPostData.py
@@ -0,0 +1,40 @@
+from thirdweb import ThirdwebSDK
+import random
+from thirdweb.types import SDKOptions
+from dotenv.main import load_dotenv
+import os
+
+# Set your RPC_URL
+RPC_URL = "https://binance-testnet.rpc.thirdweb.com"
+
+# And now you can instantiate the SDK with it
+sdk = ThirdwebSDK(RPC_URL)
+
+load_dotenv()
+
+# This PRIVATE KEY is coming from your environment variables. Make sure to never put it in a tracked file or share it with anyone.
+private_key = ''
+secret_key = ""
+
+# Now you can create a new instance of the SDK with your private key
+sdk = ThirdwebSDK.from_private_key(private_key, RPC_URL, options=SDKOptions(secret_key=secret_key))
+contract = sdk.get_contract("abcdef")
+
+#GET DATA
+# call to function of contract to get data
+
+tokenId = 6
+data = contract.call("getNFTInfo", tokenId)
+print(data)
+
+#POST DATA
+# write on blockchain (can observe changes at https://testnet.bscscan.com/address/0x7e15935f8ae6FCbBAc0211D2E15A2166143707B3)
+
+to = "0x5360D8952D040e1d2D3d9941485C0E4d8B2F22fF"
+roomId = random.randint(0,9)
+rentAmount = random.randint(0,999)
+secondsUntilStartTime = random.randint(3600,7200)
+duration = random.randint(7200,10000)
+
+data = contract.call("safeMint", to, roomId, rentAmount, secondsUntilStartTime, duration)
+
